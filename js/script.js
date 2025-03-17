2efb46140f57b54e15fecf84933d87b8111064dc
Rahidul Islam <rahiseli@outlook.com>
2025-03-17 09:00:32 +0600

    faq right is done.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-document.querySelectorAll("accordian-title").forEach(title=>{
-    title.addEventListener("click",function(){
-        this.parentElement.classList.toggle("active")
+const faqs = document.querySelectorAll(".faq-item")
+
+faqs.forEach((faq)=>{
+    faq.addEventListener("click",()=>{
+        const icon = faq.querySelector("svg")
+        faq.classList.toggle("active")
+        if (icon.classList.contains("fa-plus")){
+            icon.classList.replace("fa-plus","fa-xmark")
+        }else{
+            icon.classList.replace("fa-xmark","fa-plus")
+        }
     })
 })
